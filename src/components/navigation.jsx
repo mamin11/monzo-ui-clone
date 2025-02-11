99be799a0797a45ba45bcaefa5663a3df85d0770
Amin <abdimamin@gmail.com>
2025-02-11 21:25:02 +0000

    chore: add menu to navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,11 +1,15 @@
+import Menu from "./menu";
 
 export default function Navigation() {
     return (
-        <div className="w-full h-24 pt-4 pb-8 pl-[17rem] pr-[17rem]">
+        <div className="sticky top-0 bg-[#F2F8F3]/80 backdrop-filter backdrop-blur-sm max-w-[90rem] max-h-[700px] mx-auto h-24 pt-4 pb-8 pl-[2rem] pr-[2rem]">
             <div className="flex justify-between">
-                <div className="text-4xl text-red-500 font-bold">wonzo</div>
+                <div className="flex flex-row space-x-4">
+                    <div className="text-4xl text-red-500 font-bold">wonzo</div>
+                    <Menu />
+                </div>
                 <div className="flex space-x-4">
-                    <button className="text-lg text-white font-semibold bg-[#016B83] px-6 py-3 rounded-lg">Sign up</button>
+                    <button className="text-lg text-white font-semibold bg-[#016B83] pl-[1.4rem] pr-[1.4rem] py-3 rounded-lg">Sign up</button>
                 </div>
             </div>
         </div>
