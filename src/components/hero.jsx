78573eafe0ad103016439fac9d7f0250b78b4447
Amin <abdimamin@gmail.com>
2025-02-11 21:25:21 +0000

    chore: update hero image position

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function HeroComponent() {
     return (
-        <div className="w-full mt-1 pl-[14.5rem] pr-[15.5rem]">
-            <Image src="/hero.png" width={1920} height={1080} className="rounded-2xl mb-10" />
+        <div className="w-full mt-1 xl:pl-[14.5rem] xl:pr-[15.5rem] 2xl:px-28">
+            <div className="relative max-w-[90rem] max-h-[700px] mx-auto rounded-2xl mb-10">
+                <Image alt="hero" src="/hero.png" width={1920} height={1080} className="rounded-2xl mb-10" />
+                <div className="absolute h-full top-0 left-0 text-center grid grid-cols-2">
+                    <div className="flex flex-col h-full justify-center text-start space-y-4 pl-10 pt-10 align-middle">
+                        <h1 className=" text-6xl font-bold text-white">Make your money <br /> more monzo</h1>
+                        <p className="text-white text-lg font-thin tracking-wide">Automate bills. Organise how you spend. Save in clever <br/> new ways. All with a free Monzo Current Account.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
